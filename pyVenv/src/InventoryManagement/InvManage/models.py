e70821ac6d05df3d5c620e63b3e185c038d65c8b
artichock <artichock113@gmail.com>
2020-02-23 19:24:21 +0530

    Product class creation

--- pyVenv/src/InventoryManagement/InvManage/models.py
+++ pyVenv/src/InventoryManagement/InvManage/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Dashboard(models.Model):
+    
+    
+    
+    
+class Product(models.Model):
+    CATEGORY=
+    ProductName = models.CharField(max_length=50)
+    ProductCategory = models.CharField(choice=CATEGORY,)
+    ProductPrice = models.FloatField()
+    ProductQuantity = models.IntegerField()
+    ProductImage = models.ImageField(upload=)
+    ProductLength = models.FloatField()
+    ProductWidth = models.FloatField()
+    ProductHeight = models.FloatField()
+    ProductDescription = models.TextField()
+
+
+
+class Purchase(models.Model):
+    VendorName = models.CharField(max_length=100)
+    VendorPhone = models.CharField(max_legnth=10)
+    VendorAdd = models.TextField()
+
+
+
+
+
+class Sale(models.Model):
+
