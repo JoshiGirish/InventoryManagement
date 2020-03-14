a4efe1fae1dea5a135615cc7fda3d65ee8715a73
joshigirish <girishjoshi135@gmail.com>
2020-03-15 01:12:04 +0530

    Added basic_form, detailed_form, images, statuses, pricing_info, dimensions, and storage_info. Also added icons.

---
+++ pyVenv/src/InventoryManagement/InvManage/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+from .models import Product
+
+# def basic_info_form():
+#     name = models.CharField(max_length=100)
+#     item_type = models.CharField(max_length=100)
+#     category = models.CharField(max_length=100)
+#     description = models.TextField()
+
+
+
+class ProductBasicInfoForm(forms.Form):
+    context={
+        "class": "form-control",
+    }
+    # Basic Information form fields
+    name = forms.CharField(widget=forms.TextInput(attrs=context))
+    item_type = forms.CharField(widget=forms.TextInput(attrs=context))
+    category = forms.CharField(widget=forms.TextInput(attrs=context))
+    description = forms.CharField(widget=forms.Textarea(
+        attrs={
+            "class": "form-control",
+            "rows": 3
+        }
+    ))
+
+class ProductDetailedInfoForm(forms.Form):
+    context={
+        "class": "form-control",
+    }
+    # Dimensions form fields
+    length = forms.CharField(widget=forms.TextInput(attrs=context))
+    width = forms.CharField(widget=forms.TextInput(attrs=context))
+    height = forms.CharField(widget=forms.TextInput(attrs=context))
+    weight = forms.CharField(widget=forms.TextInput(attrs=context))
+
+
+class ProductThumbnailForm(forms.Form):
+    image = forms.ImageField()
+    # class Meta:
+    #     model = Product
+    #     fields = ['name','category','item_type','description']
+
+    # class ProductDetailedDimensionsForm(forms.Form):
+
+class ProductStorageInfoForm(forms.Form):
+    barcode = forms.ImageField()
+    context={
+        "class": "form-control",
+        "type": "date"
+    }
+    expiry = forms.DateField(widget=forms.widgets.DateInput(attrs=context))
+
+class ProductPricingForm(forms.Form):
+    context={
+        "class": "form-control",
+    }
+    price = forms.FloatField(label="MRP",widget=forms.NumberInput(attrs={'id': 'form_homework', 'step': "1", "class": "form-control"}))
+    discount = forms.FloatField(widget=forms.NumberInput(attrs={'id': 'form_homework', 'step': "1", "class": "form-control"}))
