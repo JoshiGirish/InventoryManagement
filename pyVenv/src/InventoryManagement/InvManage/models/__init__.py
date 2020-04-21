ee846d21071a14d3834b6a7b043239d36819b662
joshigirish <girishjoshi135@gmail.com>
2020-04-22 00:43:21 +0530

    Fixes #27. Pagination controls are now displayed correctly.

---
+++ pyVenv/src/InventoryManagement/InvManage/models/__init__.py
@@ -0,0 +1,3 @@
+from .objects import Product, ProductPurchaseEntry, PurchaseOrder, Vendor, Company, ShippingAddress, Invoice
+from .states import ProductFilterColumn, ProductFilterState
+__all__ = ['Product', 'ProductPurchaseEntry', 'PurchaseOrder','Vendor','Company','ShippingAddress','Invoice','ProductFilterColumn','ProductFilterState']
