d84bdcec9f23f7094d00e85b3c2df9bb76bcc94f
joshigirish <girishjoshi135@gmail.com>
2021-09-25 00:16:42 +0530

    Remove extra files.

--- pyVenv/src/InventoryManagement/InventoryManagement/settings.py
+++ pyVenv/src/InventoryManagement/InventoryManagement/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [config('ALLOWED_HOST'), config('LOCALHOST')]
+ALLOWED_HOSTS = [config('ALLOWED_HOST'), config('LOCALHOST'), "*"]
 
 
 # Application definition
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/static'
+STATIC_ROOT = '/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'InventoryManagement/assets/'),
     os.path.join(BASE_DIR, 'InventoryManagement/assets/css/'),
@@ -171,14 +171,14 @@ if DEBUG:
     
     
 # AWS S3 Bucket configuration    
-AWS_ACCESS_KEY_ID = config('S3_SECRET_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('S3_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('S3_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = config('S3_REGION')
-AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3-{AWS_S3_REGION_NAME}.amazonaws.com"
+# AWS_ACCESS_KEY_ID = config('S3_SECRET_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = config('S3_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = config('S3_STORAGE_BUCKET_NAME')
+# AWS_S3_REGION_NAME = config('S3_REGION')
+# AWS_S3_FILE_OVERWRITE = False
+# # AWS_DEFAULT_ACL = None
+# AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3-{AWS_S3_REGION_NAME}.amazonaws.com"
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
